3f2d624659194608893f4011c32871af7e8e9814
Dmitry Savelyev <ds2012.65@gmail.com>
2019-06-16 03:21:40 +0300

    fix db setup

--- engines/quotas/app/decorators/models/cluster_decorator.rb
+++ engines/quotas/app/decorators/models/cluster_decorator.rb
@@ -1,4 +1,4 @@
 Core::Cluster.class_eval do
   has_many :cluster_quota_kinds, class_name: "Quotas::ClusterQuotaKind", inverse_of: :cluster, dependent: :destroy
   has_many :quotas, class_name: "Quotas::Quota", as: :domain, dependent: :destroy
-end
+end unless File.basename($0) == 'rake'
